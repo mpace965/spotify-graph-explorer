8f3f58eea9fd2ec63b992ef663ad72c62bead7c6
Matt Pace <pace4@purdue.edu>
2017-01-20 23:03:41 -0500

    Add search endpoint

--- app.rb
+++ app.rb
@@ -13,6 +13,18 @@ get '/search' do
   content_type :json
 
   artists = RSpotify::Artist.search(params['artist'])
+  artist = artists.first
+  artist_hash = artist_to_artist_hash artist
 
-  artists.to_json
+  artist_hash[:related_artists] = artist.related_artists.map { |a| artist_to_artist_hash a }
+
+  artist_hash.to_json
+end
+
+def artist_to_artist_hash artist
+  {
+    id: artist.id,
+    name: artist.name,
+    images: artist.images
+  }
 end
