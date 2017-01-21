24e0cce193e235423179ae50799ec1bed96cb3ab
Matt Pace <pace4@purdue.edu>
2017-01-21 10:48:52 -0500

    Refactor Graph component to be dumb

--- javascript/App.jsx
+++ javascript/App.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Navbar } from 'react-bootstrap'
+import $ from 'jquery'
+import _ from 'lodash'
 
 import Graph from './Graph.jsx'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      artistAndRelated: {}
+    }
+  }
+
+  getArtistAndRelated(artist) {
+    $.ajax({
+      type: 'get',
+      url: '/search',
+      data: { artist },
+      success: artistAndRelated => {
+        this.setState({ artistAndRelated })
+      },
+      error: response => {
+        console.log(response)
+      }
+    })
   }
 
   renderNavbar() {
@@ -25,7 +45,10 @@ class App extends React.Component {
     return (
       <div style={{ height: '100%', overflow: 'hidden' }}>
         {this.renderNavbar()}
-        <Graph />
+        <Graph
+          artistAndRelated={this.state.artistAndRelated}
+          getArtistAndRelated={artist => this.getArtistAndRelated(artist)}
+        />
       </div>
     )
   }
--- javascript/Graph.jsx
+++ javascript/Graph.jsx
@@ -1,52 +1,49 @@
 import React from 'react'
 import cytoscape from 'cytoscape'
-import $ from 'jquery'
-import _ from 'lodash'
 
 class Graph extends React.Component {
   cy = null
 
   componentDidMount() {
+    const { getArtistAndRelated } = this.props
+
     let cy = cytoscape({
       container: document.getElementById('cy'),
       elements: [],
     })
 
-    cy.layout({ name: 'circle' })
+    this.cy = cy
 
-    this.cy = cy;
-
-    this.getArtistAndRelated('kanye')
+    getArtistAndRelated('kanye')
   }
 
-  getArtistAndRelated(artist) {
-    $.ajax({
-      type: 'get',
-      url: '/search',
-      data: { artist },
-      success: artistAndRelated => {
-        const { artist, related_artists } = artistAndRelated
+  componentDidUpdate(prevProps) {
+    const { artistAndRelated } = this.props
+    const prevArtistAndRelated = prevProps.artistAndRelated
 
-        this.cy.add({ data: { id: artist.id } })
+    if (artistAndRelated !== prevArtistAndRelated) {
+      this.addArtistsToGraph()
+    }
+  }
 
-        _.forEach(related_artists, relatedArtist => {
-          this.cy.add({ data: { id: relatedArtist.id } })
+  addArtistsToGraph() {
+    const { artist, related_artists } = this.props.artistAndRelated
 
-          this.cy.add({
-            data: {
-              id: `${artist.id}-${relatedArtist.id}`,
-              source: artist.id,
-              target: relatedArtist.id
-            }
-          })
-        })
+    this.cy.add({ data: { id: artist.id } })
 
-        this.cy.layout({ name: 'circle' })
-      },
-      error: response => {
-        console.log(response)
-      }
+    _.forEach(related_artists, relatedArtist => {
+      this.cy.add({ data: { id: relatedArtist.id } })
+
+      this.cy.add({
+        data: {
+          id: `${artist.id}-${relatedArtist.id}`,
+          source: artist.id,
+          target: relatedArtist.id
+        }
+      })
     })
+
+    this.cy.layout({ name: 'circle' })
   }
 
   render() {
