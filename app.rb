da0c7aced29695bd239b8a79295535cdbe43a5c3
Matt Pace <pace4@purdue.edu>
2017-01-20 22:49:22 -0500

    Add rspotify and set up test route

--- app.rb
+++ app.rb
@@ -1,8 +1,18 @@
 require 'sinatra'
+require 'rspotify'
+require 'json'
 
 set :haml, format: :html5
 set :public_folder, 'public'
 
 get '/' do
   haml :index
+end
+
+get '/search' do
+  content_type :json
+
+  artists = RSpotify::Artist.search(params['artist'])
+
+  artists.to_json
 end
