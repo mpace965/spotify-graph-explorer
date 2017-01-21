0ee1ba0f83ed2d9bc9cc244dbc77f31bb972622a
Matt Pace <pace4@purdue.edu>
2017-01-21 16:50:24 -0500

    Set up authorization flow and change theme

--- app.rb
+++ app.rb
@@ -2,14 +2,28 @@ require 'sinatra'
 require 'rspotify'
 require 'json'
 require 'dotenv'
+require 'omniauth'
+require 'rspotify/oauth'
 
 Dotenv.load
 
+set :sessions, true
 set :haml, format: :html5
 set :public_folder, 'public'
 
+RSpotify::authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
+
+use OmniAuth::Builder do
+  provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'], scope: 'playlist-modify-public'
+end
+
 get '/' do
   haml :index
+end
+
+get '/auth/spotify/callback' do
+  session[:spotify_user] = request.env['omniauth.auth']
+  redirect '/'
 end
 
 get '/search' do
--- javascript/Search.jsx
+++ javascript/Search.jsx
@@ -24,7 +24,7 @@ class Search extends React.Component {
   render() {
     return (
       <form onSubmit={e => this.onSearch(e)} style={{ width: '400px' }}>
-        <FormGroup style={{ paddingTop: '7px', marginBottom: '0px' }}>
+        <FormGroup style={{ paddingTop: '12px', marginBottom: '0px' }}>
           <FormControl
             type='text'
             value={this.state.term}
