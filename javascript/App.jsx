e7a45e634efeac49a350e9b54cfaca8db83c3dd2
Matt Pace <pace4@purdue.edu>
2017-01-21 00:33:53 -0500

    Add Graph component

--- javascript/App.jsx
+++ javascript/App.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { render } from 'react-dom'
-
 import { Navbar } from 'react-bootstrap'
 
+import Graph from './Graph.jsx'
+
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      elements: []
+    }
+  }
+
   renderNavbar() {
     return (
       <Navbar>
         <Navbar.Header>
           <Navbar.Brand>
-            <a href="#">Spotify Graph Explorer</a>
+            <a href='#'>Spotify Graph Explorer</a>
           </Navbar.Brand>
         </Navbar.Header>
       </Navbar>
@@ -17,8 +26,13 @@ class App extends React.Component {
   }
 
   render() {
+    const { elements } = this.state
+
     return (
-      this.renderNavbar()
+      <div>
+        {this.renderNavbar()}
+        <Graph elements={elements} />
+      </div>
     )
   }
 }
---
+++ javascript/Graph.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Graph extends React.Component {
+  render() {
+    return <p> Hi </p>
+  }
+}
+
+export default Graph
