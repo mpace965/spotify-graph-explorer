c0216ad625235bde13bc263c15bf151b4565f0f0
Matt Pace <pace4@purdue.edu>
2017-02-04 02:57:51 -0500

    Change to const

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,10 +1,10 @@
-var webpack = require('webpack')
-var path = require('path')
+const webpack = require('webpack')
+const path = require('path')
 
-var BUILD_DIR = path.resolve(__dirname, 'public')
-var APP_DIR = path.resolve(__dirname, 'client')
+const BUILD_DIR = path.resolve(__dirname, 'public')
+const APP_DIR = path.resolve(__dirname, 'client')
 
-var config = {
+const config = {
   devtool: 'eval',
   entry: APP_DIR + '/components/App.jsx',
   output: {
