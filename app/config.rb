086f41cdc3fa0fde8eb9470584e73cd73ab11292
Matt Pace <pace4@purdue.edu>
2017-02-05 18:59:01 -0500

    Better gzip check

--- app/config.rb
+++ app/config.rb
@@ -13,4 +13,12 @@ use OmniAuth::Builder do
   provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'], scope: 'playlist-modify-public playlist-modify-private'
 end
 
-use Rack::Deflater
+use Rack::Deflater, if: (lambda do |env, status, headers, body|
+  if body.respond_to? :filesize
+    body.filesize > 512
+  elsif body.is_a?(Array) && body.first.is_a?(String)
+    body.map(&:bytesize).reduce(0, :+) > 512
+  else
+    true
+  end
+end)
